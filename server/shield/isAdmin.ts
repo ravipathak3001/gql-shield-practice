adc349d0ddcb9499236a40149797eede5f844842
ravipathak3001 <ravipathak3001@gmail.com>
2023-03-05 12:00:16 +0530

    updated

--- server/shield/isAdmin.ts
+++ server/shield/isAdmin.ts
@@ -1,8 +1,5 @@
 import { rule } from 'graphql-shield';
 export default rule({ cache: 'contextual' })(async (_parent, _args, ctx) => {
   const userRoles = ctx.user;
-  console.log('====================================');
-  console.log(userRoles);
-  console.log('====================================');
   return userRoles.includes('ADMIN');
 });
